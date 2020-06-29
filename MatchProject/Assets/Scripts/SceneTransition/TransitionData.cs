d335395a92c3c6446b9498fec672f4553ca5a212
bruno takashi <bt.tengan@gmail.com>
2020-06-29 05:53:28 -0300

    Initial UI and scene transition setup

---
+++ MatchProject/Assets/Scripts/SceneTransition/TransitionData.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Transition_data", menuName = "ScriptableObjects/Transition Data")]
+public class TransitionData : ScriptableObject {
+	public GameObject transition = null;
+}
---
+++ MatchProject/Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuController : MonoBehaviour {
+	[SerializeField] GameObject _menu = null;
+
+	void Start() {
+		if (!_menu) {
+			Debug.LogWarning("No Menu specified to control");
+		}
+		else {
+			_menu.SetActive(false);
+		}
+	}
+
+	void OnDestroy() {
+		Time.timeScale = 1;
+	}
+
+	public void Activate() {
+		_menu.SetActive(true);
+		Time.timeScale = 0;
+	}
+
+	public void Deactivate() {
+		_menu.SetActive(false);
+		Time.timeScale = 1;
+	}
+}
