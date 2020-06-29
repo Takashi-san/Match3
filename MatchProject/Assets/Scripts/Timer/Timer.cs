3e2fa6d80ba41809fa36746cc214f2b2a4e74216
bruno takashi <bt.tengan@gmail.com>
2020-06-29 00:12:53 -0300

    Timer

---
+++ MatchProject/Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+
+public class Timer : MonoBehaviour {
+	[SerializeField] [Min(0)] float _StartTime = 0;
+
+	public Action<float> timerUpdate;
+	public Action timerEnd;
+
+	float _timer = 0;
+	bool _active = false;
+
+	void Awake() {
+		_timer = _StartTime;
+	}
+
+	void Start() {
+		if (timerUpdate != null) {
+			timerUpdate(_timer);
+		}
+		StartTimer();
+	}
+
+	void Update() {
+		if (_active) {
+			_timer -= Time.deltaTime;
+			if (_timer <= 0) {
+				_timer = 0;
+				_active = false;
+				if (timerEnd != null)
+					timerEnd();
+			}
+			if (timerUpdate != null)
+				timerUpdate(_timer);
+		}
+	}
+
+	public void StartTimer() {
+		_active = true;
+	}
+
+	public void StopTimer() {
+		_active = false;
+	}
+}
---
+++ MatchProject/Assets/Scripts/Timer/TimerDisplay.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TimerDisplay : MonoBehaviour {
+	TextMeshProUGUI _text;
+
+	void Awake() {
+		_text = gameObject.GetComponent<TextMeshProUGUI>();
+		FindObjectOfType<Timer>().timerUpdate += UpdateText;
+	}
+
+	void UpdateText(float timer) {
+		int minutes = Mathf.FloorToInt(timer / 60);
+		int seconds = Mathf.FloorToInt(timer % 60);
+		_text.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+	}
+}
