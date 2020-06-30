782703ba39c25e67d12a1198b84a80149e1eed00
bruno takashi <bt.tengan@gmail.com>
2020-06-30 01:55:03 -0300

    Music and general SFX

---
+++ MatchProject/Assets/Scripts/SfxPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SfxPlayer : MonoBehaviour {
+	[SerializeField] AudioMixerGroup _sfxMixerGroup;
+
+	AudioSource _audioSource;
+	SfxPlayer _instance;
+	public SfxPlayer Instance => _instance;
+
+	void Awake() {
+		if (_instance == null) {
+			_instance = this;
+		}
+		else {
+			Debug.LogWarning("Duplicate of SfxPlayer in: " + gameObject.name);
+			Destroy(this);
+		}
+
+		_audioSource = gameObject.AddComponent<AudioSource>();
+		_audioSource.outputAudioMixerGroup = _sfxMixerGroup;
+	}
+
+	public void PlaySfx(AudioClip sfx) {
+		_audioSource.PlayOneShot(sfx);
+	}
+}
---
+++ MatchProject/Assets/Scripts/UI/ButtonPlaySfx.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ButtonPlaySfx : MonoBehaviour {
+	SfxPlayer _source;
+
+	void Start() {
+		_source = FindObjectOfType<SfxPlayer>();
+	}
+
+	public void PlaySfx(AudioClip sfx) {
+		if (_source != null)
+			_source.PlaySfx(sfx);
+	}
+}
